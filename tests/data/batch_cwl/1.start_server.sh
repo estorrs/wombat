5e6112fb80598d27e10cc353c1b1c010eb836f07
Erik <epstorrs@gmail.com>
2022-02-08 10:45:17 -0600

    fix hello cwl

--- tests/data/batch_cwl/1.start_server.sh
+++ tests/data/batch_cwl/1.start_server.sh
@@ -3,4 +3,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/batch_cwl:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/batch_cwl /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
 bgadd -L 25 /estorrs/test_cromwell_hello
-bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell_hello -J 7f4d2ae7-a056-4778-af3d-15c4a9b7c162 -Is '/bin/bash'
+bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell_hello -J cf91fdcd-e20e-4c73-b243-9c3dca7bbc5f -Is '/bin/bash'
