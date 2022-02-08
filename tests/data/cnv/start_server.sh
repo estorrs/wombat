40e0a2e3c3141957a9c7a556f2daabd5f5e5c3d2
Erik <epstorrs@gmail.com>
2022-02-08 10:22:11 -0600

    new cnv

--- tests/data/cnv/start_server.sh
+++ tests/data/cnv/start_server.sh
@@ -4,4 +4,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/cnv:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/cnv /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/cnv/C3L-00677:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/cnv/C3L-00677 /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
 bgadd -L 10 /estorrs/test_cromwell
-bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J 628df2c5-9dbb-4b5b-be14-aebfc4b51334 -Is '/bin/bash'
+bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J a58cb4fc-3c36-41ff-81d7-656c4b93699a -Is '/bin/bash'
