99900bc70afcb516edee3831dcda2f6fb0669879
Erik <epstorrs@gmail.com>
2022-02-08 09:06:39 -0600

    new hello batch and cnv workflow

--- tests/data/batch_cwl/1.start_server.sh
+++ tests/data/batch_cwl/1.start_server.sh
@@ -9,4 +9,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/batch_cwl:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/batch_cwl /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
 bgadd -L 25 /estorrs/test_cromwell_hello
-bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell_hello -J 312e02e2-be04-4491-8fed-162f6b00c7f9 -Is '/bin/bash'
+bsub -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell_hello -J db72e8ae-ce80-46d6-b429-1c2610388e70 -Is '/bin/bash'
