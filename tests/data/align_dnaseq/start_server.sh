cbc3523b6c823d609fa523d7c20b344f9fc8668c
Erik <epstorrs@gmail.com>
2022-02-14 14:39:13 -0600

    t

---
+++ tests/data/align_dnaseq/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/align_dnaseq/HT191P1-S1H1A3Y3/cromwell-workdir/logs
+source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
+export LSF_DOCKER_NETWORK=host
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/align_dnaseq:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/align_dnaseq /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/align_dnaseq/HT191P1-S1H1A3Y3:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/align_dnaseq/HT191P1-S1H1A3Y3 /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
+bgadd -L 10 /estorrs/test_cromwell
+bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J eeb1362d-5f9a-46e6-b3c7-838f4c2ffc30 -Is '/bin/bash'
