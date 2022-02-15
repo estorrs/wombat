bc174d36b2d340d2855ceccd9c5e5434d3ebc9e0
Erik <epstorrs@gmail.com>
2022-02-15 10:46:28 -0600

    fix vcf issue

--- tests/data/align_dnaseq/start_server.sh
+++ tests/data/align_dnaseq/start_server.sh
@@ -4,4 +4,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/align_dnaseq:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/align_dnaseq /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/align_dnaseq/HT191P1-S1H1A3Y3:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/align_dnaseq/HT191P1-S1H1A3Y3 /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
 bgadd -L 10 /estorrs/test_cromwell
-bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J cd192545-124d-4b14-abaa-519f8f0b8334 -Is '/bin/bash'
+bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J d4a8d391-03d0-4312-9e94-a11adc0da9de -Is '/bin/bash'
