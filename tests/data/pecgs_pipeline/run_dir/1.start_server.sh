895377c4399e1d852ff33641fa31f39bb54cdac5
Erik <epstorrs@gmail.com>
2022-02-21 17:59:48 -0600

    fix rna filepath issue

--- tests/data/pecgs_pipeline/run_dir/1.start_server.sh
+++ tests/data/pecgs_pipeline/run_dir/1.start_server.sh
@@ -3,4 +3,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/pecgs:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/pecgs /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
 bgadd -L 50 /estorrs/default_pecgs_pipeline
-bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/default_pecgs_pipeline -J b70f2017-e818-43e2-bfa8-009a831b08a6 -Is '/bin/bash'
+bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/default_pecgs_pipeline -J 2d110521-711d-4124-a450-18571e974131 -Is '/bin/bash'
