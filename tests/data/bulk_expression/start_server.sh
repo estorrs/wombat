3c21875a7171b978c18bb516863d0fbcc41faa7f
Erik <epstorrs@gmail.com>
2022-03-10 11:10:31 -0600

    add bulk expression test

---
+++ tests/data/bulk_expression/start_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/bulk_expression/C3L-00677/cromwell-workdir/logs
+source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
+export LSF_DOCKER_NETWORK=host
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/bulk_expression:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/bulk_expression /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/bulk_expression/C3L-00677:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/bulk_expression/C3L-00677 /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
+bgadd -L 10 /estorrs/test_cromwell
+bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J c384cc13-1603-4d9f-a27f-3728148093be -Is '/bin/bash'
