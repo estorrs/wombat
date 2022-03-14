b5c0ea9be9a9bad87ddd329938ff63869f2348f9
Erik <epstorrs@gmail.com>
2022-03-14 14:19:07 -0500

    bugfix

--- wombat/utils.py
+++ wombat/utils.py
@@ -42,7 +42,7 @@ def parse_output_from_log(log_fp, workflow_name):
         if '"location": ' in line and identifier is not None:
             location = re.sub(r'^.*location.*"(.*)".*$', r'\1', line.strip())
             if is_list and indent - 2 == section_indent:
-                logging(f'extracting location from list: {location}')
+                logging.info(f'extracting location from list: {location}')
                 m[identifier].append(location)
             elif not is_list:
                 logging.info(f'extracting location: {location}')
