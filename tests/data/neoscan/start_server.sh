54d1a0dbf75edf14acb4bdbd8202f3816fdc0240
Erik <epstorrs@gmail.com>
2022-03-23 11:38:39 -0500

    pipeline updates

--- tests/data/neoscan/start_server.sh
+++ tests/data/neoscan/start_server.sh
@@ -4,4 +4,4 @@ source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
 export LSF_DOCKER_NETWORK=host
 export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/neoscan:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/neoscan /storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/pecgs-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/neoscan/C3L-00677:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/neoscan/C3L-00677 /storage1/fs1/dinglab/Active:/storage1/fs1/dinglab/Active /storage1/fs1/m.wyczalkowski/Active:/storage1/fs1/m.wyczalkowski/Active /scratch1/fs1/dinglab/estorrs:/scratch1/fs1/dinglab/estorrs"
 bgadd -L 10 /estorrs/test_cromwell
-bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J 0a751cfa-9903-4f3c-866c-784f422cae03 -Is '/bin/bash'
+bsub -n 1 -q dinglab-interactive -G compute-dinglab -a 'docker(mwyczalkowski/cromwell-runner)' -g /estorrs/test_cromwell -J 17ffa883-8f0f-4533-a7b4-dd0bd9758077 -Is '/bin/bash'
