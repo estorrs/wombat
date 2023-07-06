0c5cc920884e1d7c77a8bbab5ef0978947d64cec
Erik Storrs <epstorrs@gmail.com>
2023-07-06 07:53:27 -0500

    adding spatial features test

---
+++ tests/data/spatial_features/run_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/spatial_features/cromwell-workdir/logs
+source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
+export LSF_DOCKER_NETWORK=host
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/spatial_features:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/spatial_features /storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/spatial_features:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/spatial_features /storage1/fs1/dinglab:/storage1/fs1/dinglab /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
+bgadd -L 10 /estorrs/test_cromwell
+export PATH="/opt/java/openjdk/bin:$PATH"
+bsub -R 'select[mem>10GB] rusage[mem=10GB] span[hosts=1]' -M 11GB -n 1 -q dinglab -G compute-dinglab -a 'docker(estorrs/cromwell-runner:58)' -g /estorrs/test_cromwell -J f21bc6d9-8580-4502-94a2-523341b772e3 -oo log.txt '/opt/java/openjdk/bin/java -Dconfig.file=/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/spatial_features/spatial_features.cromwell-config-db.compute1.dat -jar /app/cromwell-78-38cd360.jar run -t cwl -i /storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/spatial_features/inputs_spatial_features.yaml /storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline/cwl/spatial_features.cwl'
