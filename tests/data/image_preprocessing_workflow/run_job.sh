c6a15437d36bae763a6156b17c7b168cc14ec831
Erik Storrs <epstorrs@gmail.com>
2023-07-06 10:37:47 -0500

    first test

---
+++ tests/data/image_preprocessing_workflow/run_job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+mkdir -p /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/image_preprocessing_workflow/cromwell-workdir/logs
+source /opt/ibm/lsfsuite/lsf/conf/lsf.conf
+export LSF_DOCKER_NETWORK=host
+export LSF_DOCKER_VOLUMES="/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/image_preprocessing_workflow:/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/image_preprocessing_workflow /storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline:/storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline /scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/image_preprocessing_workflow:/scratch1/fs1/dinglab/estorrs/cromwell-data/pecgs/testing/image_preprocessing_workflow /storage1/fs1/dinglab:/storage1/fs1/dinglab /scratch1/fs1/dinglab:/scratch1/fs1/dinglab"
+bgadd -L 10 /estorrs/test_cromwell
+export PATH="/opt/java/openjdk/bin:$PATH"
+bsub -R 'select[mem>10GB] rusage[mem=10GB] span[hosts=1]' -M 11GB -n 1 -q dinglab -G compute-dinglab -a 'docker(estorrs/cromwell-runner:58)' -g /estorrs/test_cromwell -J 4f5cdfec-fa0b-471b-a08e-714d37e4b5c2 -oo log.txt '/opt/java/openjdk/bin/java -Dconfig.file=/storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/image_preprocessing_workflow/image_preprocessing_workflow.cromwell-config-db.compute1.dat -jar /app/cromwell-78-38cd360.jar run -t cwl -i /storage1/fs1/dinglab/Active/Projects/estorrs/wombat/tests/data/image_preprocessing_workflow/inputs.yaml /storage1/fs1/dinglab/Active/Projects/estorrs/multiplex-imaging-pipeline/cwl/image_preprocessing_workflow.cwl'
